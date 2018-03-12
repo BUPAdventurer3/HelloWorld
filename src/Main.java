a4cf1f42484d4be724bc356dffd23a70a6734acc
hridibupict <anisatasnim@yahoo.com>
2018-03-12 13:00:18 +0600

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Scanner in =  new Scanner (System.in);
+
+        String s;
+        s =  in.nextLine();
+
+        System.out.println (s);
+
     }
 }
