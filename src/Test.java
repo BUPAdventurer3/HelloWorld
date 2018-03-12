dd0f841819f0ea0e070cb07edbc4d1561a1e2bb1
hridibupict <anisatasnim@yahoo.com>
2018-03-12 13:48:14 +0600

    add & Subtract Commit

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,10 @@
 public class Test {
+
+    public static void main (String args [])
+    {
+        int a = 3, b = 5;
+
+        System.out.println ("Addition: " +(a+b));
+        System.out.println ("Subtraction: " +(a-b));
+    }
 }
